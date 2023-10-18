b6948bd4363e0f59fb0ba94d8e8e58810b47b654
victor <victor.h@clarusway.com>
2023-10-18 11:44:53 +0200

    add modals footer

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,59 +1,80 @@
-import useLoginModal from "@/hooks/useLoginModal"
-import { useCallback, useState } from "react"
-import Input from "../Input"
-import Modal from "../Modal"
+import useLoginModal from "@/hooks/useLoginModal";
+import { useCallback, useState } from "react";
+import Input from "../Input";
+import Modal from "../Modal";
+import useRegisterModal from "@/hooks/useRegisterModal";
 
 const LoginModal = () => {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
 
-    const loginModal = useLoginModal()
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
+  const { isLoading, setIsLoading } = useState(false);
 
-    const {email, setEmail} = useState("")
-    const {password, setPassword} = useState("")
-    const {isLoading, setIsLoading} = useState(false)
+  const onToggle = useCallback(() => {
+    if (isLoading) {
+      return;
+    }
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [isLoading, registerModal, loginModal]);
 
-    const onSubmit = useCallback(async () => {
-        try {
-            setIsLoading(true)
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
 
-            // TODO: add login
+      // TODO: add login
 
-            loginModal.onClose()
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setIsLoading(false)
-        }
-    },[loginModal])
+      loginModal.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [loginModal]);
 
-    const bodyContent = (
-        <div className="flex flex-col gap-4">
-            <Input
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            disabled={isLoading}
-            />
-            <Input
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            disabled={isLoading}
-            />
-        </div>
-    )
-    
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4 flex mx-auto">
+      <p>First time using Twitter?</p>
+      <span
+        onClick={onToggle}
+        className="text-white cursor-pointer hover:underline ml-1"
+      >
+        Create an Account
+      </span>
+    </div>
+  );
 
   return (
     <Modal
-    disabled={isLoading}
-    isOpen={loginModal.isOpen}
-    title="Login"
-    actionLabel="Sign in"
-    onClose={loginModal.onClose}
-    onSubmit={onSubmit}
-    body={bodyContent}
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Sign in"
+      onClose={loginModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+      footer={footerContent}
     />
-  )
-}
+  );
+};
 
-export default LoginModal
+export default LoginModal;
